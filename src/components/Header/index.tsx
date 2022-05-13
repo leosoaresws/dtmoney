801c6e2084541539c1264a8266dbff6387a7f9c4
Leonardo Soares <leonardo.soares.ws@gmail.com>
2022-05-13 00:01:54 -0300

    Consumindo a api e criando o modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,18 @@
 import { Container, Content } from "./style";
 import logoImg from "../../assets/logo.svg";
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money logo" />
-        <button type="button">Nova Transação</button>
+        <button type="button" onClick={onOpenNewTransactionModal}>
+          Nova Transação
+        </button>
       </Content>
     </Container>
   );
